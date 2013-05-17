1fd4cb206198db6ffb75dd2a138552752ca33bf5
Evgeni <evgenikostadinov@gmail.com>
2013-05-17 17:59:15 +0300

    14.DevelpmentToolsHW

---
+++ QA/Homework/14.DevelopmentTools/SimpleT4Template/MyClass.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleT4Template
+{
+	class MyClass
+	{
+				public int Item0 { get; set; }
+				public int Item1 { get; set; }
+				public int Item2 { get; set; }
+				
+		public int SumThreeNumbers(int one, int two, int three)
+		 {
+			var sum = 0;
+			
+			sum += one;                
+			sum += two;
+			sum += three;
+
+			return sum;
+	    }
+    
+		//I couldn't find how to pass the parameters when call the method
+		//SumThreeNumbers(1, 2, 3);
+		
+	}
+}
---
+++ QA/Homework/14.DevelopmentTools/UseDevelopmentTolls/MainUseProgram.cs
@@ -0,0 +1,23 @@
+﻿
+namespace UseDevelopmentTolls
+{
+    using System;
+    using System.Linq;
+
+    class MainUseProgram
+    {
+        static void Main()
+        {
+            //1. I generally use Git source control system, You can find one of my repos at:
+            https://github.com/ekostadinov/CoursesProjects
+
+            //3. I have installed both StyleCop and JustCode
+            //also they were used on this very same homework - screenshots can be found in the 'images' folder
+
+            //4. I personally prefer to use ILSpy
+
+            //5. I use Sandcastle on my PC - screenshot can be found in the 'images' folder
+            //and .shfbproj file in the solution folder
+        }
+    }
+}
